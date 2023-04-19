f2ae94d32760f31bdd11e23d552cca0e696849ff
aoreo <hay6586@gmail.com>
2023-04-20 08:55:40 +0900

    backapi

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,16 +1,34 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 import React, { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View, Modal, Alert, Pressable } from "react-native";
 function Main() {
   const [modalVisible, setModalVisible] = useState(false);
+  const API_URL = "http://127.0.0.1:5000";
   const load = async () => {
     try {
       const value = await AsyncStorage.getItem('myData');
       console.log("value : ",value);
       setModalVisible(!modalVisible);
+      if(value!=null){
+        await api(value);
+      }else{
+        console.log("value is null")
+      }
+     
     } catch (e) {
       // 오류 예외 처리
     }
+  }
+  const api =async(value:string)=>{
+    await axios.post(
+      `${API_URL}/todo`,
+      {value}
+    ).then((res)=>{
+      console.log(res.data)
+    }).catch((err)=>{
+      console.log(err)
+    })
   }
    return(
       <View style={styles.centeredView}>
@@ -24,7 +42,6 @@ function Main() {
         visible={modalVisible}
         onRequestClose={() => {
           Alert.alert('Modal has been closed.');
-          
         }}>
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
