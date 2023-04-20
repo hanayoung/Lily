1e8fd9a2e0bcd1cfe67e6d5770eb1f8260115832
aoreo <hay6586@gmail.com>
2023-04-20 14:13:10 +0900

    api

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,41 +1,100 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
-import React, { useState } from "react";
+import moment from "moment";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View, Modal, Alert, Pressable } from "react-native";
 function Main() {
   const [modalVisible, setModalVisible] = useState(false);
+  const [todo,setTodo] = useState("");
+  const [comfort,setComfort] = useState("");
+  const [advice, setAdvice] = useState("");
+  const [isChanged,setIsChanged] = useState(false);
+
   const API_URL = "http://127.0.0.1:5000";
-  const load = async () => {
+  
+  const load = () => {
     try {
-      const value = await AsyncStorage.getItem('myData');
-      console.log("value : ",value);
-      setModalVisible(!modalVisible);
-      if(value!=null){
-        await api(value);
-      }else{
-        console.log("value is null")
-      }
-     
+      setModalVisible(!modalVisible);   
     } catch (e) {
       // 오류 예외 처리
     }
   }
-  const api =async(value:string)=>{
+  const getTodoApi = async(value:string)=>{
     await axios.post(
       `${API_URL}/todo`,
       {value}
     ).then((res)=>{
-      console.log(res.data)
+      console.log(res.data.text)
+      setTodo(res.data.text)
+      setIsChanged(true)
     }).catch((err)=>{
       console.log(err)
     })
   }
+  
+  const getComfortApi =async (value:string) => {
+    await axios.post(
+      `${API_URL}/comfort`,
+      {value}
+    ).then((res)=>{
+      console.log(res.data.text)
+      setComfort(res.data.text)
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
+  
+
+  const getAdviceApi =async (value:string) => {
+    await axios.post(
+      `${API_URL}/advice`,
+      {value}
+    ).then((res)=>{
+      console.log(res.data.text)
+      setAdvice(res.data.text)
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
+  
+
+  const isKeyExists =async (today:string) => {
+    const isKeyExist = await AsyncStorage.getItem(today);
+    if(isKeyExist!==null){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  useEffect(()=>{
+    const today = moment().format('MM-DD');
+    const checkKeyExist=async () => {
+      const isKeyExist = await isKeyExists(today);
+    if(!isKeyExist){
+      getUserData();
+    }else{
+      console.log("it already has!")
+    }
+    }
+    const getUserData = async () => {
+      const value = await AsyncStorage.getItem('myData');
+      if(value!=null){
+        await getTodoApi(value);
+      }else{
+        console.log("value is null")
+      }
+    }
+    checkKeyExist();
+  },[isChanged])
+
    return(
       <View style={styles.centeredView}>
     <TouchableOpacity 
     onPress={()=>load()}>
       <Text>Click!</Text>
     </TouchableOpacity>
+    <Text>{todo}</Text>
     <Modal
         animationType="slide"
         transparent={true}
